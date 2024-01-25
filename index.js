43e8d88edc6b5cc6b6c6c9edf33cb6e9aadde0a9
Wyyxx <a1121120071@utch.edu.mx>
2024-01-25 09:40:54 -0600

    Primer Contacto

---
+++ index.js
@@ -0,0 +1,22 @@
+const app = require("express");
+const server = app();
+
+
+server.put("/:valor", (req,res) =>{
+
+})
+
+server.post("/:valor:index", (req,res) =>{
+
+})
+
+server.delete("/:index", (req,res) =>{
+
+})
+
+server.get("/:index",(req,res) =>{
+
+})
+server.listen(3001,() => {
+    console.log("Server is running")
+    })
