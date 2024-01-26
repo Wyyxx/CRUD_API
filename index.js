63a25d9356331b2b4ad7468a8fa3ab71e5ef5663
Wyyxx <a1121120071@utch.edu.mx>
2024-01-26 09:57:43 -0600

    Cambio del Index

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-const app = require("express");
-const server = app();
+const express = require('express');
+const app = express();
+const port = 3000;
+
+let data = [
+    { id: 1, name: 'Spiderman', Personaje: 'Peter Parker' },
+    { id: 2, name: 'Iron Man', Personaje: 'Tony Stark' },
+    { id: 3, name: 'Guardianes de la Galaxia', Personaje: 'Star Lord' },
+    { id: 4, name: 'Avengers', Personaje: 'Tanatos' },
+];
+
+app.use(express.json());
+app.use(express.static('public'));
 
 
-server.put("/:valor", (req,res) =>{
 
-})
+// PUT update data by id
+app.put('/data/:id', (req, res) => {
+    const item = data.find(i => i.id === parseInt(req.params.id));
+    if (!item) return res.status(404).send('Item not found');
+    item.name = req.body.name;
+    item.Personaje = req.body.Personaje;
+    res.json(item);
+});
 
-server.post("/:valor:index", (req,res) =>{
 
-})
-
-server.delete("/:index", (req,res) =>{
-
-})
-
-server.get("/:index",(req,res) =>{
-
-})
-server.listen(3001,() => {
-    console.log("Server is running")
-    })
+app.listen(port, () => {
+    console.log(`Server is running in http://localhost:${port}`);
+});
