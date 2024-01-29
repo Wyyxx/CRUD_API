dddc28bffa9b0a86f7a6deea61d885d8b43f9a3b
michmorales29 <michellemrls2912@outlook.com>
2024-01-29 08:16:45 -0600

    modified

--- index.js
+++ index.js
@@ -12,39 +12,35 @@ let data = [
 app.use(express.json());
 app.use(express.static('public'));
 
-<<<<<<< Updated upstream
 
-=======
-server.put("/:valor", (req,res) =>{
->>>>>>> Stashed changes
 
-// PUT update data by id
-app.put('/data/:id', (req, res) => {
-    const item = data.find(i => i.id === parseInt(req.params.id));
-    if (!item) return res.status(404).send('Item not found');
-    item.name = req.body.name;
-    item.Personaje = req.body.Personaje;
-    res.json(item);
-});
+    // PUT update data by id
+    app.put('/data/:id', (req, res) => {
+        const item = data.find(i => i.id === parseInt(req.params.id));
+        if (!item) return res.status(404).send('Item not found');
+        item.name = req.body.name;
+        item.Personaje = req.body.Personaje;
+        res.json(item);
+    });
 
-//delete data por id
-app.delete('/data/:id',(req, res)=>{
-    const itemId = parseInt(req.params.id);
-    data=data.filter(item =>item.id !== itemId)
-    res.send("Datos con el id ${itemId} han sido eliminados");
-});
+    //delete data por id
+    app.delete('/data/:id',(req, res)=>{
+        const itemId = parseInt(req.params.id);
+        data=data.filter(item =>item.id !== itemId)
+        res.send("Datos con el id ${itemId} han sido eliminados");
+    });
 
-//delete data por nombre
-app.delete('/data/:name',(req, res)=>{
-    const itemName = req.params.name; 
-    if (!itemName) return res.status(400).send('Parámetros invalidos');
+    //delete data por nombre
+    app.delete('/data/name/:name',(req, res)=>{
+        const itemName = req.params.name; 
+        if (!itemName) return res.status(400).send('Parámetros invalidos');
 
-    data = data.filter(item => item.name !== itemName);
-    res.send("Datos con el nombre ${itemName} han sido eliminados")
-});
+        data = data.filter(item => item.name !== itemName);
+        res.send("Datos con el nombre ${itemName} han sido eliminados")
+    });
 
 
 
-app.listen(port, () => {
-    console.log(`Server is running in http://localhost:${port}`);
+    app.listen(port, () => {
+    console.log('Server is running in http://localhost:${port}');
 });
