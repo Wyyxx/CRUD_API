8ea8a8d84362abeb33cd60d6cce91f82fb712d20
Wyyxx <a1121120071@utch.edu.mx>
2024-01-29 21:47:07 -0600

    Correcion de POST

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
 const express = require('express');
 const app = express();
+const port = 3000;
+
+let data = [
+    { id: 1, name: 'Spiderman', Personaje: 'Peter Parker' },
+    { id: 2, name: 'Iron Man', Personaje: 'Tony Stark' },
+    { id: 3, name: 'Guardianes de la Galaxia', Personaje: 'Star Lord' },
+    { id: 4, name: 'Avengers', Personaje: 'Tanatos' },
+    { id: 5, name: 'Hulk', personaje: 'Bruce Banner' },
+    { id: 6, name: 'Black Widow', personaje: 'Natasha Romanoff' },
+    { id: 7, name: 'Thor', personaje: 'Thor Odinson' },
+    { id: 8, name: 'Captain America', personaje: 'Steve Rogers' },
+    { id: 9, name: 'Doctor Strange', personaje: 'Stephen Strange' },
+    { id: 10, name: 'Wolverine', personaje: 'Logan' },
+    { id: 11, name: 'Storm', personaje: 'Ororo Munroe' },
+    { id: 12, name: 'Deadpool', personaje: 'Wade Wilson' },
+    { id: 13, name: 'Scarlet Witch', personaje: 'Wanda Maximoff' },
+    { id: 14, name: 'Ante', personaje: 'Scott Lang' }
+];
 
 app.use(express.json());
+app.use(express.static('public'));
 
-let peliculas = [];
-let idPelicula = 1;
-
-app.post('/peliculas', (req, res) => {
-    const nuevaPelicula = { id: idPelicula++, ...req.body };
-    peliculas.push(nuevaPelicula);
-    res.json({ mensaje: 'Película agregada con éxito', pelicula: nuevaPelicula });
+app.post('/personajes', (req, res) => {
+    const maxId = Math.max(...data.map(item => item.id));
+    const nuevoPersonaje = { id: maxId + 1, name: req.body.name, Personaje: req.body.Personaje };
+    data.push(nuevoPersonaje);
+    res.json({ mensaje: 'Personaje agregado con éxito', personaje: nuevoPersonaje });
 });
 
-app.post('/peliculas/:id', (req, res) => {
-    const idPelicula = parseInt(req.params.id);
+app.post('/personajes/:id', (req, res) => {
+    const idPersonaje = parseInt(req.params.id);
     const datosActualizados = req.body;
 
-
-    const pelicula = peliculas.find(p => p.id === idPelicula);
-    if (pelicula) {
-        pelicula.titulo = datosActualizados.titulo || pelicula.titulo;
-        pelicula.director = datosActualizados.director || pelicula.director;
-        res.json({ mensaje: 'Información de la película actualizada con éxito', pelicula });
+    const personaje = data.find(p => p.id === idPersonaje);
+    if (personaje) {
+        personaje.name = datosActualizados.name || personaje.name;
+        personaje.Personaje = datosActualizados.Personaje || personaje.Personaje;
+        res.json({ mensaje: 'Información del personaje actualizada con éxito', personaje });
     } else {
-        res.status(404).json({ mensaje: 'Película no encontrada' });
+        res.status(404).json({ mensaje: 'Personaje no encontrado' });
     }
 });
 
